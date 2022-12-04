0fc84d2a2f3ad097d0e1bd0df18920ac0bff5603
Thomas Crawford <ThomasJamesCrawford96@gmail.com>
2022-12-04 14:11:15 +0800

    day4

---
+++ 2022/day04/main_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+const SAMPLE string = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		input io.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "sample",
+			args: args{input: strings.NewReader(SAMPLE)},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		input io.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "sample",
+			args: args{input: strings.NewReader(SAMPLE)},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.args.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
